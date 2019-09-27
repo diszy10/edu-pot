ae6bd64ca3875110a70438d6bb2ac52a1c122b46
diszy10 <pradisz.co@gmail.com>
2019-09-27 17:46:00 +0700

    Database service init

---
+++ lib/core/services/database_service.dart
@@ -0,0 +1,12 @@
+import 'package:edukasi_pot/core/services/stoppable_service.dart';
+
+class DatabaseService implements StoppableService {
+  @override
+  bool get serviceStopped => null;
+
+  @override
+  void start() {}
+
+  @override
+  void stop() {}
+}
